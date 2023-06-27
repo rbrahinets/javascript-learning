f40a8da652b91af150258017da696603384b721c
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-27 18:43:51 +0300

    Add function for getting course by id

--- nodejs-express-restapi-tutorial/express/index.js
+++ nodejs-express-restapi-tutorial/express/index.js
@@ -10,5 +10,9 @@ app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
