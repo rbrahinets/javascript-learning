ad8aa40e64c931d87fbd81b7c2670106a8af080d
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-27 22:41:59 +0300

    Add router handler for delete request

--- nodejs-express-restapi-tutorial/express/index.js
+++ nodejs-express-restapi-tutorial/express/index.js
@@ -70,6 +70,20 @@ app.put('/api/courses/:id', (req, res) => {
     res.send(course);
 });
 
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (!course) {
+        res.status(404).send(`The course with ID '${req.params.id}' was not found!`);
+        return;
+    }
+    
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
