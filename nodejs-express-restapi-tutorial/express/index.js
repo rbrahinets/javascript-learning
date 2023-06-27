210492406dcd8c64c93fa2038f7a36dd327eb6f9
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-27 18:58:56 +0300

    Add router handler for post request

--- nodejs-express-restapi-tutorial/express/index.js
+++ nodejs-express-restapi-tutorial/express/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     {id: 1, name: 'course1'},
     {id: 2, name: 'course2'},
@@ -24,6 +26,16 @@ app.get('/api/courses/:id', (req, res) => {
     } else {
         res.send(course);
     }    
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
 });
 
 const port = process.env.PORT || 8080;
