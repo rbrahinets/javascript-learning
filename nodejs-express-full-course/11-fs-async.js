21553fcb0343d708b6f8110735fe7dfc2b864c5c
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-28 16:44:49 +0300

    Add files for async work with fs

---
+++ nodejs-express-full-course/11-fs-async.js
@@ -0,0 +1,32 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, res) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    const first = res;
+
+    readFile('./content/second.txt', 'utf8', (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const second = res;
+
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}\n`,
+            (err, res) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                console.log(res);
+            }
+        );
+    });
+});
