b16667350707ad26331d90b6bef5c4ddbd72b0ee
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-28 19:08:21 +0300

    Add file '2-setTimeout.js'

---
+++ nodejs-expressjs-full-course/nodejs/1-event-loop/2-setTimeout.js
@@ -0,0 +1,9 @@
+// started operating system process
+console.log('First');
+
+setTimeout(() => {
+    console.log('Second');
+}, 0);
+
+console.log('Third');
+// completed and exited operating system process
