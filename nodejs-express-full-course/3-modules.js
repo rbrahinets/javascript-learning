f00d3a4bdcaf91c6eb507bd58e4f520576a16539
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-28 15:54:18 +0300

    Add file for alternative work with modules

--- nodejs-express-full-course/3-modules.js
+++ nodejs-express-full-course/3-modules.js
@@ -3,7 +3,9 @@
 
 const { john, peter } = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
 
 sayHi('Susan');
 sayHi(john);
 sayHi(peter);
+sayHi(data.singlePerson.name);
