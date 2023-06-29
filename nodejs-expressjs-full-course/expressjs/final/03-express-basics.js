ba6cf9c6633a312605334bf012a1311a23097571
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-29 19:23:59 +0300

    Add file '03-express-basics.js'

---
+++ nodejs-expressjs-full-course/expressjs/final/03-express-basics.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    console.log('User hit the resource');
+    res.status(200).send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resource not found!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
