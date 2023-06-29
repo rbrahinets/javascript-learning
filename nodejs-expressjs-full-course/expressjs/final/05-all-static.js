d1516d2519396bda2451cad302a0851ca8d06574
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-29 19:43:34 +0300

    Add file '05-all-static.js'

---
+++ nodejs-expressjs-full-course/expressjs/final/05-all-static.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 8080;
+
+// setu static and middleware
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//     res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+//     adding to static assete
+//     SSR
+// });
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Resource not found!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+});
