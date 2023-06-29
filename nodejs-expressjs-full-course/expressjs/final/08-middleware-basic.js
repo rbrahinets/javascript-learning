1dcf0c2940ba5965468752c8e83bc6e87771f9c5
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-29 22:50:23 +0300

    Add file '08-middleware-basic.js'

---
+++ nodejs-expressjs-full-course/expressjs/final/08-middleware-basic.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+// req => middleware => res
+
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+
+    console.log(method, url, time);
+
+    next();
+};
+
+app.get('/', logger, (req, res) => {
+    res.send('Home');
+});
+
+app.get('/about', logger, (req, res) => {
+    res.send('About');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
