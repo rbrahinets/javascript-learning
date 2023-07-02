d8cf1f7fbe0eb8ef664f8709dfc9298a9886e962
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-02 17:58:18 +0300

    Add file '11-methods.js'

---
+++ nodejs-expressjs-full-course/expressjs/final/11-methods.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const { people } = require('./final/data');
+
+const app = express();
+
+const PORT = 8080;
+
+// static assets
+app.use(express.static('./methods-public'));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+// parse json
+app.use(express.json());
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please provide name value'
+        });
+    }
+
+    res.status(201).send({
+        success: true,
+        person: name
+    });
+});
+
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please provide name value'
+        });
+    }
+
+    res.status(201).send({
+        success: true,
+        data: [...people, name]
+    });
+});
+
+app.post('/login', (req, res) => {
+    // console.log(req.body);
+
+    const { name } = req.body;
+
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`);
+    }
+
+    res.status(401).send('Please Provide Credentials');
+});
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const person = people.find(person => person.id === Number(id));
+
+    if (!person) {
+        return res.status(404).json({
+            success: false,
+            message: `No person with id ${id}`
+        });
+    }
+
+    const newPeople = people.map(person => {
+        if (person.id === Number(id)) {
+            person.name = name;
+        }
+
+        return person;
+    });
+
+    res.status(200).send({
+        success: true,
+        data: newPeople
+    });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+    const { id } = req.params;
+
+    const person = people.find(person => person.id === Number(id));
+
+    if (!person) {
+        return res.status(404).json({
+            success: false,
+            message: `No person with id ${id}`
+        });
+    }
+
+    const newPeople = people.filter(person => person.id !== Number(id));
+
+    res.status(200).send({
+        success: true,
+        data: newPeople
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
