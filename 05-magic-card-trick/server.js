a0ea43b5ef1dc2cfd4e9a0c42f43b2fe0de63997
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-05 18:47:32 +0300

    Update script for server

--- 05-magic-card-trick/server.js
+++ 05-magic-card-trick/server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
-const mongo = require('./config/db');
+const PlayerModel = require('./models/player.model');
 
-const URI = process.env.MONGODB_URI || mongo.url;
 const PORT = process.env.PORT || 8080;
-const DB_NAME = process.env.DB_NAME;
 
 const app = express();
-const MongoClient = mongodb.MongoClient;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,59 +15,54 @@ app.get('/secret', (req, res) => {
 });
 
 app.post('/secret', (req, res) => {
-    MongoClient.connect(URI, { useNewUrlParser: true }, (err, client) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const db = client.db(DB_NAME);
-            const collection = db.collection('names');
-            const entry = {
-                name: req.body.name.toLowerCase(),
-                card: `${req.body.number}_of_${req.body.suit}`,
-            };
+    if (!req.body) {
+        return res.status(400).send('Request body is missing');
+    }
 
-            collection.insertOne(entry, (err, result) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    res.send('Inserted into database');
-                }
-            });
-
-            db.close();
-        }
+    const model = new PlayerModel({
+        name: req.body.name.toLowerCase(),
+        card: `${req.body.number}_of_${req.body.suit}`,
     });
+    model
+        .save()
+        .then((doc) => {
+            if (!doc || doc.length === 0) {
+                return res.status(500).send(doc);
+            }
+
+            res.status(201).send(doc);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 app.get('/:param*', (req, res) => {
     const name = req.url.slice(1).toLowerCase();
 
-    MongoClient.connect(URI, { useNewUrlParser: true }, (err, client) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const db = client.db(DB_NAME);
-            const collection = db.collection('names');
-
-            if (name === 'deleteall') {
-                collection.remove({});
+    if (name === 'deleteall') {
+        PlayerModel.deleteMany({})
+            .then((doc) => {
                 res.send('database reset');
-            } else {
-                collection.find({ name: name }).toArray((err, result) => {
-                    if (err) {
-                        console.error(err);
-                    } else if (result.length) {
-                        const card = `${result[result.length - 1].card}.png`;
-                        res.sendFile(path.join(`${__dirname}/cards/${card}`));
-                    } else {
-                        res.sendStatus(404);
-                    }
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    } else {
+        PlayerModel.findOne({ name: name })
+            .then((doc) => {
+                if (!doc) {
+                    return res.status(404).send('Page Not Found');
+                }
 
-                    db.close();
-                });
-            }
-        }
-    });
+                const card = `${doc.card}.png`;
+                res.sendFile(path.join(`${__dirname}/cards/${card}`));
+            })
+            .catch((err) => {
+                console.error('Error ', err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 app.listen(PORT, () => {
