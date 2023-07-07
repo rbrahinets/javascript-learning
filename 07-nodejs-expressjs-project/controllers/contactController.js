3dcc7252ba1cc5b17807c40fc5f98c93022b52cc
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-07 16:35:36 +0300

    Add model controller for conatcts

---
+++ 07-nodejs-expressjs-project/controllers/contactController.js
@@ -0,0 +1,69 @@
+const asyncHandler = require('express-async-handler');
+const Contact = require('../models/contactModels');
+
+const getContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+});
+
+const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact Not Found');
+    }
+
+    res.status(200).json(contact);
+});
+
+const createContact = asyncHandler(async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error('All fields are mandory');
+    }
+
+    const contact = await Contact.create({ name, email, phone });
+
+    res.status(201).json(contact);
+});
+
+const updateContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact Not Found');
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.status(200).json(updatedContact);
+});
+
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+        res.status(404);
+        throw new Error('Contact Not Found');
+    }
+
+    await Contact.findByIdAndDelete(req.params.id);
+
+    res.status(200).json(contact);
+});
+
+module.exports = {
+    getContacts,
+    getContact,
+    createContact,
+    updateContact,
+    deleteContact,
+};
